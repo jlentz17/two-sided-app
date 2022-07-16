6001331d499068f9589bab4669ee916e8e5718ad
jlentz17 <64111084+jlentz17@users.noreply.github.com>
2022-07-15 22:55:12 -0700

    add method-override & create delete route & action

--- passport-config.js
+++ passport-config.js
@@ -1,8 +1,8 @@
 const LocalStrategy = require('passport-local').Strategy
 const bcrypt = require('bcrypt')
 
-function initialize(passport, getUserByEmail) {
-  const authenticateUser = async (email, passport, done) => {
+function initialize(passport, getUserByEmail, getUserById) {
+  const authenticateUser = async (email, password, done) => {
     const user = getUserByEmail(email)
     if (user == null) {
       return done(null, false, { message: 'No user with that email' })
@@ -19,8 +19,10 @@ function initialize(passport, getUserByEmail) {
     }
   }
   passport.use(new LocalStrategy({ usernameField: 'email' }, authenticateUser))
-  passport.serializeUser((user, done) => {})
-  passport.deserializeUser((id, done) => {})
+  passport.serializeUser((user, done) => done(null, user.id))
+  passport.deserializeUser((id, done) => {
+    return done(null, getUserById(id))
+  })
 }
 
 module.exports = initialize
--- server.js
+++ server.js
@@ -8,11 +8,14 @@ const bcrypt = require('bcrypt')
 const passport = require('passport')
 const flash = require('express-flash')
 const session = require('express-session')
+const methodOverride = require('method-override')
 
 const initializePassport = require('./passport-config')
-initializePassport(passport, (email) => {
-  return users.find((user) => user.email === email)
-})
+initializePassport(
+  passport,
+  (email) => users.find((user) => user.email === email),
+  (id) => users.find((user) => user.id === id)
+)
 
 const users = []
 
@@ -28,17 +31,19 @@ app.use(
 )
 app.use(passport.initialize())
 app.use(passport.session())
+app.use(methodOverride('_method'))
 
-app.get('/', (req, res) => {
-  res.render('index.ejs', { name: 'Jeremy' })
+app.get('/', checkAuthenticated, (req, res) => {
+  res.render('index.ejs', { name: req.user.name })
 })
 
-app.get('/login', (req, res) => {
+app.get('/login', checkNotAuthenticated, (req, res) => {
   res.render('login.ejs')
 })
 
 app.post(
   '/login',
+  checkNotAuthenticated,
   passport.authenticate('local', {
     successRedirect: '/',
     failureRedirect: '/login',
@@ -46,11 +51,11 @@ app.post(
   })
 )
 
-app.get('/register', (req, res) => {
+app.get('/register', checkNotAuthenticated, (req, res) => {
   res.render('register.ejs')
 })
 
-app.post('/register', async (req, res) => {
+app.post('/register', checkNotAuthenticated, async (req, res) => {
   try {
     const hashedPassword = await bcrypt.hash(req.body.password, 10)
     users.push({
@@ -65,5 +70,29 @@ app.post('/register', async (req, res) => {
   }
   console.log(users)
 })
+
+app.delete('/logout', function (req, res, next) {
+  req.logOut(function (err) {
+    if (err) {
+      return next(err)
+    }
+    res.redirect('/login')
+  })
+})
+
+function checkAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next()
+  }
+
+  return res.redirect('/login')
+}
+
+function checkNotAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return res.redirect('/')
+  }
+  return next()
+}
 
 app.listen(3000)
